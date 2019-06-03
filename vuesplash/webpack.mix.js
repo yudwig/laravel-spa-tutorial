9303efbd9a338a714dfc7953c6840669862ea12f
yudwig <yudwig.dev@gmail.com>
2019-06-03 20:51:23 +0900

    add support for another dev-env

--- vuesplash/webpack.mix.js
+++ vuesplash/webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.browserSync('vuesplash.sass')
+mix.browserSync('vuesplash.test')
 	.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
 	.version();
