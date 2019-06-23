e56de1873988d4d28e10b5c8967ea4998e667fcd
yudwig <yudwig.dev@gmail.com>
2019-06-23 13:35:18 +0900

    chapter 6 end.

--- vuesplash/resources/js/router.js
+++ vuesplash/resources/js/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import PhotoList from './pages/PhotoList.vue';
 import Login from './pages/Login.vue';
+import store from './store';
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -11,7 +13,15 @@ const routes = [
 	},
 	{
 		path: '/login',
-		component: Login
+		component: Login,
+        beforeEnter(to, from, next) {
+		    // ログイン済みなのにURL直指定でログイン画面にきた場合はトップページに移動させる。
+		    if (store.getters['auth/check']) {
+		        next('/');
+            } else {
+		        next();
+            }
+        }
 	}
 ];
 
